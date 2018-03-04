77687f57e5783072eab807b4a41320bc3bb8aa5b
Leo-taro FUJIMOTO <lfujimot@e3r12p6.42.fr>
2018-03-04 09:56:38 +0100

    ok

--- dirasm/new_instr.c
+++ dirasm/new_instr.c
@@ -6,21 +6,13 @@
 /*   By: ssi-moha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/03 18:21:05 by ssi-moha          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2018/03/03 18:53:06 by lfujimot         ###   ########.fr       */
-=======
-/*   Updated: 2018/03/03 18:21:09 by ssi-moha         ###   ########.fr       */
->>>>>>> refs/remotes/origin/master
+/*   Updated: 2018/03/04 09:56:17 by lfujimot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_corewar.h"
 
-<<<<<<< HEAD
 t_instr	*new_instr(char *cmd, t_instr **prev)
-=======
-void	new_instr(char *cmd, t_instr **prev)
->>>>>>> refs/remotes/origin/master
 {
 	t_instr *tmp;
 	t_instr *tmp2;
@@ -28,11 +20,7 @@ void	new_instr(char *cmd, t_instr **prev)
 	tmp = NULL;
 	tmp2 = *prev;
 	if (!(tmp = (t_instr*)malloc(sizeof(t_instr))))
-<<<<<<< HEAD
 		return (0);
-=======
-		return ;
->>>>>>> refs/remotes/origin/master
 	tmp->cmd = ft_strdup(cmd);
 	tmp->label = NULL;
 	tmp->params = NULL;
@@ -45,8 +33,5 @@ void	new_instr(char *cmd, t_instr **prev)
 			tmp2 = tmp2->next;
 		tmp2->next = tmp;
 	}
-<<<<<<< HEAD
 	return (tmp);
-=======
->>>>>>> refs/remotes/origin/master
 }
--- includes/ft_corewar.h
+++ includes/ft_corewar.h
@@ -5,12 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lfujimot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/03/03 17:02:23 by lfujimot          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2018/03/03 18:54:51 by lfujimot         ###   ########.fr       */
-=======
-/*   Updated: 2018/03/03 18:22:55 by ssi-moha         ###   ########.fr       */
->>>>>>> refs/remotes/origin/master
+/*   Created: 2018/03/04 09:53:57 by lfujimot          #+#    #+#             */
+/*   Updated: 2018/03/04 09:54:04 by lfujimot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +20,9 @@
 # include "../gnl/get_next_line.h"
 # include "../libft/libft.h"
 # include "../printf/includes/ft_printf.h"
-<<<<<<< HEAD
 # include "op.h"
-
-=======
 # include <stdio.h>
->>>>>>> refs/remotes/origin/master
+
 typedef struct		s_par
 {
 	char			*par;
@@ -43,18 +36,11 @@ typedef struct		s_instr
 	t_par			*params;
 	struct s_instr	*next;
 }					t_instr;
-<<<<<<< HEAD
+
 int					ft_isincharset(char c, char *charset);
 int					ft_free_par(t_par **par);
 void				new_par(char *par, t_par **prev);
 t_instr				*new_instr(char *cmd, t_instr **prev);
 int					free_instr(t_instr **instr);
 int					ft_parselabel(char *line, t_instr *new);
-=======
-
-int		free_par(t_par **par);
-void	new_par(char *par, t_par **prev);
-void	new_instr(char *cmd, t_instr **prev);
-int		free_instr(t_instr **instr);
->>>>>>> refs/remotes/origin/master
 #endif
