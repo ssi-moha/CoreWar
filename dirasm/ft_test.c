2bbb2b92ab0ab0ed4c6543d564d11c1fa262c3f1
Leo-taro FUJIMOTO <lfujimot@e3r12p6.42.fr>
2018-03-05 19:52:29 +0100

    problem

--- dirasm/ft_test.c
+++ dirasm/ft_test.c
@@ -6,7 +6,7 @@
 /*   By: emerabet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 14:25:49 by emerabet          #+#    #+#             */
-/*   Updated: 2018/03/05 19:14:24 by lfujimot         ###   ########.fr       */
+/*   Updated: 2018/03/05 19:47:56 by lfujimot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,14 @@ int		ft_testhex(t_app *application)
 	instructions = application->instr;
 	
 	printf("\n\n TEST WRITE HEADER\n");
+//	write(out, &header.magic, sizeof(unsigned int));
+	printf("HEXXXX %x\n", header.magic);
+//	write(out, header.prog_name, sizeof(char * (PROG_NAME_LENGHT + 1)));
+//	write(out, header.prog_size, sizeof(unsigned int));
+//	write(out, header.comment, sizeof(char * (COMMENT_LENGTH + 1)));
 //	write(out, application->header.comment, COMMENT_LENGTH + 1);
 	write(out, &header, sizeof(t_header));
+	write(out, "\0\0\0\0", 4);
 	printf("%s\n", header.prog_name);
 	printf("%s\n", header.comment);
 	ft_printhex(instructions, out);
