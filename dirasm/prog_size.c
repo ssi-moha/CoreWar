460403995e3c271ee03749f88118a87e35f7ac28
Sinane SI MOHAMMED <ssi-moha@e2r11p10.42.fr>
2018-03-06 11:54:19 +0100

    fct prog size

---
+++ dirasm/prog_size.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prog_size.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssi-moha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/06 11:51:01 by ssi-moha          #+#    #+#             */
+/*   Updated: 2018/03/06 11:53:57 by ssi-moha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ft_corewar.h"
+
+int		prog_size(t_instr **instr)
+{
+	t_instr *tmp;
+	int i;
+
+	i = 0;
+	if (!*instr)
+		return (0);
+	tmp = *instr;
+	while (tmp)
+	{
+		i += tmp->sizeoctet;
+		tmp = tmp->next;
+	}
+	return (i);
+}
