5c165d7dd0a280f586604554bac6ae66498319a3
Sinane SI MOHAMMED <ssi-moha@e1r1p12.42.fr>
2018-03-08 13:57:58 +0100

    Norm

--- dirasm/ft_findcmd.c
+++ dirasm/ft_findcmd.c
@@ -6,16 +6,16 @@
 /*   By: lfujimot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 12:05:41 by lfujimot          #+#    #+#             */
-/*   Updated: 2018/03/05 12:10:38 by lfujimot         ###   ########.fr       */
+/*   Updated: 2018/03/08 13:56:42 by ssi-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_corewar.h"
 
-int	ft_findcmd(char *cmd)
+int		ft_findcmd(char *cmd)
 {
 	int i;
-	
+
 	i = 0;
 	while (i < 17)
 	{
