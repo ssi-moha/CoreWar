ac0f239a71165252b922e01bcad52f10c08059b4
Leo-taro FUJIMOTO <lfujimot@e1r1p9.42.fr>
2018-03-08 14:16:01 +0100

    l

--- dirasm/ft_printdir.c
+++ dirasm/ft_printdir.c
@@ -6,13 +6,36 @@
 /*   By: lfujimot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/08 13:59:38 by lfujimot          #+#    #+#             */
-/*   Updated: 2018/03/08 13:59:40 by lfujimot         ###   ########.fr       */
+/*   Updated: 2018/03/08 14:15:18 by lfujimot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_corewar.h"
 
-void		ft_printdir(t_par *par, int cmd, int out)
+static unsigned char	ft_mod(unsigned int *tp)
+{
+	unsigned char p;
+
+	p = *tp % 256;
+	*tp /= 256;
+	return (p);
+}
+
+static void				ft_initp(int *d, int *i, int *nb, unsigned int *tp)
+{
+	*d = 0;
+	*i = DIR_SIZE - 1;
+	if (*nb >= 0)
+		*tp = *nb;
+}
+
+static void				ft_dirshort(int nb, unsigned int *tp)
+{
+	if (nb < 0)
+		*tp = 65536 + nb;
+}
+
+void					ft_printdir(t_par *par, int cmd, int out)
 {
 	unsigned char	p[DIR_SIZE];
 	int				nb;
@@ -20,36 +43,23 @@ void		ft_printdir(t_par *par, int cmd, int out)
 	int				d;
 	unsigned int	tp;
 
-	d = 0;
-	i = DIR_SIZE - 1;
 	nb = ft_atoi(par->par + 1);
-	if (nb >= 0)
-		tp = nb;
+	ft_initp(&d, &i, &nb, &tp);
 	if (g_optab[cmd].dirsize == 0)
 	{
 		if (nb < 0)
 			tp = 4294967296 + nb;
 		while (i >= 0)
-		{
-			p[i--] = tp % 256;
-			tp /= 256;
-		}
-		d = 0;
+			p[i--] = ft_mod(&tp);
 	}
 	else if (g_optab[cmd].dirsize == 1)
 	{
-		if (nb < 0)
-			tp = 65536 + nb;
+		ft_dirshort(nb, &tp);
 		while (i >= DIR_SIZE / 2)
-		{
-			p[i--] = tp % 256;
-			tp /= 256;
-		}
+			p[i--] = ft_mod(&tp);
 		d = DIR_SIZE / 2;
 	}
 	i = d;
 	while (i < DIR_SIZE)
-	{
 		write(out, &p[i++], 1);
-	}
 }
