2e662016bc13c12091a772127698be30c2dc8e3c
Eric MERABET <emerabet@e1r1p11.42.fr>
2018-03-08 13:53:31 +0100

    count label normed

--- dirasm/count_label.c
+++ dirasm/count_label.c
@@ -6,49 +6,65 @@
 /*   By: ssi-moha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 16:20:55 by ssi-moha          #+#    #+#             */
-/*   Updated: 2018/03/08 10:51:25 by ssi-moha         ###   ########.fr       */
+/*   Updated: 2018/03/08 13:53:04 by emerabet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_corewar.h"
 
-unsigned int		count_label(t_instr **instr, t_instr **begin, char *label, int cmd)
+static unsigned int		ft_count_positive(t_instr *tmp, t_instr **instr,
+									char *label, int cmd)
 {
-	int i;
-	int mark;
-	t_instr *tmp;
-	unsigned int j;
+	int				i;
+	unsigned int	j;
+
+	i = 0;
+	while (tmp && ft_strcmp(label, tmp->label))
+	{
+		i += tmp->sizeoctet;
+		tmp = tmp->next;
+	}
+	if (tmp == 0)
+		exit(error_mess("ERROR ILLEGAL LABEL\n"));
+	j = (unsigned int)i;
+	return (j);
+}
+
+static unsigned int		ft_count_negative(t_instr *tmp, t_instr **instr,
+										int cmd)
+{
+	int				i;
+	unsigned int	j;
+
+	i = 0;
+	while (tmp && tmp != *instr)
+	{
+		i -= tmp->sizeoctet;
+		tmp = tmp->next;
+	}
+	if (g_optab[cmd].dirsize == 1)
+		j = 65536 + i;
+	else
+		j = 4294967296 + i;
+	return (j);
+}
+
+unsigned int			count_label(t_instr **instr, t_instr **begin,
+									char *label, int cmd)
+{
+	int				i;
+	t_instr			*tmp;
+	unsigned int	j;
 
 	tmp = *begin;
-	mark = 0;
 	i = 0;
-
 	while (tmp && tmp != *instr && ft_strcmp(label, tmp->label))
 		tmp = tmp->next;
 	if (tmp == *instr)
-	{
-		while (tmp && ft_strcmp(label, tmp->label))
-		{
-			i += tmp->sizeoctet;
-			tmp = tmp->next;
-		}
-		if (tmp == 0)
-			exit(error_mess("ERROR ILLEGAL LABEL\n"));
-		j = (unsigned int)i;
-	}
+		j = ft_count_positive(tmp, instr, label, cmd);
 	else if (!ft_strcmp(label, tmp->label))
-	{
-		while (tmp && tmp != *instr)
-		{
-			i -= tmp->sizeoctet;
-			tmp = tmp->next;
-		}
-		if (g_optab[cmd].dirsize == 1)
-			j = 65536 + i;
-		else
-			j = 4294967296 + i;
-	}
+		j = ft_count_negative(tmp, instr, cmd);
 	else
-		exit (error_mess("ERROR ILLEGAL LABEL\n"));
+		exit(error_mess("ERROR ILLEGAL LABEL\n"));
 	return (j);
 }
