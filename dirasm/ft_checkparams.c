168a4a19dc91978c99807baa3f1bb6ce9a621198
Eric MERABET <emerabet@e1r1p11.42.fr>
2018-03-08 14:32:04 +0100

    ok

--- dirasm/ft_checkparams.c
+++ dirasm/ft_checkparams.c
@@ -6,21 +6,59 @@
 /*   By: lfujimot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 10:42:58 by lfujimot          #+#    #+#             */
-/*   Updated: 2018/03/08 10:42:55 by ssi-moha         ###   ########.fr       */
+/*   Updated: 2018/03/08 14:31:43 by emerabet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_corewar.h"
 
+static int	ft_type_value(int dir, int lab)
+{
+	if (dir == 1 && lab == 0)
+		return (T_DIR);
+	else if (dir == 1 && lab == 1)
+		return (T_DIR + T_LAB);
+	else if (dir == 0 && lab == 0)
+		return (T_IND);
+	else if (dir == 0 && lab == 1)
+		return (T_IND + T_LAB);
+	return (-1);
+}
+
+static int	ft_check_type_except_register(char *par)
+{
+	int		i;
+	int		lab;
+	int		dir;
+
+	i = 0;
+	lab = 0;
+	dir = 0;
+	if (par[i] == DIRECT_CHAR)
+	{
+		dir = 1;
+		i++;
+	}
+	if (par[i] == LABEL_CHAR)
+		lab = 1;
+	else
+	{
+		if (par[i] == '-' || par[i] == '+')
+			i++;
+		while (par[i])
+		{
+			if (ft_isdigit(par[i++] == 0))
+				return (-1);
+		}
+	}
+	return (ft_type_value(dir, lab));
+}
+
 static int	ft_checkparamtype(char *par)
 {
-	int dir;
-	int i;
-	int lab;
+	int		i;
 
-	lab = 0;
 	i = 0;
-	dir = 0;
 	if (par)
 	{
 		if (par[i] == 'r')
@@ -29,54 +67,29 @@ static int	ft_checkparamtype(char *par)
 			while (par[i])
 			{
 				if (ft_isdigit(par[i] == 0))
-					return (-1); //erreur registre pas un nombre + check taille maximum du registre
+					return (-1);
 				i++;
 			}
 			return (T_REG);
 		}
-		else 
-		{
-			if (par[i] == DIRECT_CHAR)
-			{
-				dir = 1;
-				i++;
-			}
-			if (par[i] == LABEL_CHAR)
-				lab = 1;
-			else
-			{
-				if (par[i] == '-' || par[i] == '+')
-					i++;
-				while (par[i])
-				{
-					if (ft_isdigit(par[i] == 0))
-						return (-1); //pas un valide car pas un nombre
-					i++;
-				}
-			}
-			if (dir == 1 && lab == 0)
-				return (T_DIR);
-			else if (dir == 1 && lab == 1)
-				return (T_DIR + T_LAB);
-			else if (dir == 0 && lab == 0)
-				return (T_IND);
-			else if (dir == 0 && lab == 1)
-				return (T_IND + T_LAB);
-		}
+		else
+			return (ft_check_type_except_register(par));
 	}
 	return (-1);
 }
 
 static int	ft_checkparamvalid(char *par)
 {
-	int i;
+	int		i;
 
 	if (par)
 	{
 		i = 0;
 		if (par[i] == DIRECT_CHAR)
 			i++;
-		while (par[i] && (ft_isincharset(par[i], LABEL_CHARS) == 1 || par[i] == LABEL_CHAR ||  par[i] == DIRECT_CHAR ||  par[i] == '-'))
+		while (par[i] && (ft_isincharset(par[i], LABEL_CHARS) == 1
+					|| par[i] == LABEL_CHAR
+					|| par[i] == DIRECT_CHAR || par[i] == '-'))
 			i++;
 		if (par[i] == '\0')
 			return (1);
