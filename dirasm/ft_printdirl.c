58ec447997592cff8bc13ee4350029679b718a0d
Leo-taro FUJIMOTO <lfujimot@e1r1p9.42.fr>
2018-03-08 13:54:39 +0100

    printdirl

--- dirasm/ft_printdirl.c
+++ dirasm/ft_printdirl.c
@@ -6,41 +6,44 @@
 /*   By: lfujimot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/08 13:40:41 by lfujimot          #+#    #+#             */
-/*   Updated: 2018/03/08 13:43:30 by lfujimot         ###   ########.fr       */
+/*   Updated: 2018/03/08 13:53:52 by lfujimot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_corewar.h"
 
+static void	ft_initp(unsigned int lab, int *d, int *i, unsigned int *nb)
+{
+	*d = 0;
+	*i = DIR_SIZE - 1;
+	*nb = lab;
+}
+
 void		ft_printdiril(unsigned int lab, int cmd, int out)
 {
 	unsigned char	p[DIR_SIZE];
 	unsigned int	nb;
-	int				i;
-	int				d;
+	int				i[2];
 
-	d = 0;
-	i = DIR_SIZE - 1;
-	nb = lab;
+	ft_initp(lab, &i[1], &i[0], &nb);
 	if (g_optab[cmd].dirsize == 0)
 	{
-		while (i >= 0)
+		while (i[0] >= 0)
 		{
-			p[i--] = nb % 256;
+			p[i[0]--] = nb % 256;
 			nb /= 256;
 		}
-		d = 0;
 	}
 	else if (g_optab[cmd].dirsize == 1)
 	{
-		while (i >= DIR_SIZE / 2)
+		while (i[0] >= DIR_SIZE / 2)
 		{
-			p[i--] = nb % 256;
+			p[i[0]--] = nb % 256;
 			nb /= 256;
 		}
-		d = DIR_SIZE / 2;
+		i[1] = DIR_SIZE / 2;
 	}
-	i = d;
-	while (i < DIR_SIZE)
-		write(out, &p[i++], 1);
+	i[0] = i[1];
+	while (i[0] < DIR_SIZE)
+		write(out, &p[i[0]++], 1);
 }
