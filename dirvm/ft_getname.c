ac35891832bd8fc4b33dd68d9749aad38529134a
Eric MERABET <emerabet@e1r3p17.42.fr>
2018-03-10 12:22:58 +0100

    correction return

--- dirvm/ft_getname.c
+++ dirvm/ft_getname.c
@@ -6,13 +6,13 @@
 /*   By: emerabet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/10 11:20:05 by emerabet          #+#    #+#             */
-/*   Updated: 2018/03/10 12:20:17 by emerabet         ###   ########.fr       */
+/*   Updated: 2018/03/10 12:22:41 by emerabet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_corewar.h"
 
-void				*ft_getname(unsigned char *data, t_player *player)
+void				ft_getname(unsigned char *data, t_player *player)
 {
 	int				i;
 	int				sizemagic;
