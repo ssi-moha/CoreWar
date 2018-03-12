e2bd12eb29a14e5e5284f895b4eff528dc88808e
Leo-taro FUJIMOTO <lfujimot@e1r4p18.42.fr>
2018-03-12 13:15:49 +0100

    128

--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: lfujimot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/15 12:33:42 by lfujimot          #+#    #+#             */
-/*   Updated: 2018/02/15 16:04:21 by lfujimot         ###   ########.fr       */
+/*   Updated: 2018/03/12 13:15:30 by lfujimot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_S 1
+# define BUFF_S 128
 # include "../libft/libft.h"
 
 typedef	struct		s_file
