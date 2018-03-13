2130c54c07ede6e0008a265e38ba18ee32915981
Eric MERABET <emerabet@e1r4p17.42.fr>
2018-03-13 13:22:57 +0100

    ft_add

--- dirvm/ft_add.c
+++ dirvm/ft_add.c
@@ -6,7 +6,7 @@
 /*   By: ssi-moha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/12 14:22:01 by ssi-moha          #+#    #+#             */
-/*   Updated: 2018/03/12 14:30:43 by ssi-moha         ###   ########.fr       */
+/*   Updated: 2018/03/13 13:20:58 by emerabet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,13 @@
 
 void	ft_add(t_process *proc, t_vm *vm)
 {
-	(void)vm;
-	proc->par[2] = proc->par[0] + proc->par[1];
+	int		a;
+	int		b;
+	int		c;
+
+	a = ft_getparvalue(proc, vm, proc->par[0]);
+	b = ft_getparvalue(proc, vm, proc->par[1]);
+	c = a + b;
+	ft_writeinreg(proc, proc->par[2], c);	
 	proc->carry = 1;
 }
