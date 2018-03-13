fe6c807baf93845dafab6cd08fe84f6a3920b69f
Sinane SI MOHAMMED <ssi-moha@e1r4p19.42.fr>
2018-03-13 13:46:49 +0100

    fork

--- dirvm/ft_ld.c
+++ dirvm/ft_ld.c
@@ -6,7 +6,7 @@
 /*   By: ssi-moha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/12 12:00:41 by ssi-moha          #+#    #+#             */
-/*   Updated: 2018/03/13 13:41:33 by ssi-moha         ###   ########.fr       */
+/*   Updated: 2018/03/13 13:46:39 by ssi-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int		ft_ld(t_process *proc, t_vm *vm)
 	int i;
 
 	i = ft_getparvalue(proc, vm, proc->par[0]);
-	if (i > 16)
+	if (proc->par[1] > REG_SIZE)
 		return (0);
 	ft_writeinreg(proc, proc->par[1], i);
 	return (1);
