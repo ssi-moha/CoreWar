9533179d498f87c660f673094ef7a5ddc2c6c328
Sinane SI MOHAMMED <ssi-moha@e1r4p19.42.fr>
2018-03-13 13:01:29 +0100

    fork

--- dirvm/ft_fork.c
+++ dirvm/ft_fork.c
@@ -6,7 +6,7 @@
 /*   By: ssi-moha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/12 14:40:29 by ssi-moha          #+#    #+#             */
-/*   Updated: 2018/03/12 15:55:10 by ssi-moha         ###   ########.fr       */
+/*   Updated: 2018/03/13 13:01:18 by ssi-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,11 @@ void	ft_fork(t_process *proc, t_vm *vm)
 	player = vm->players;
 	while (player && player->number != proc->playernb)
 			player = player->next;
-	new = new_process(player, &proc, vm);
+	if ((new = new_process(player, &proc, vm)) == NULL)
+		return ;
 	new->pc = proc->pc + (proc->par[0] % IDX_MOD);
 	new->cycle = proc->cycle;
 	new->inlive = proc->inlive;
 	new->carry = proc->carry;
 	new->cmd = proc->cmd;
-	new->par[0] = proc->par[0];
-	new->par[1] = proc->par[1];
-	new->par[2] = proc->par[2];
 }
