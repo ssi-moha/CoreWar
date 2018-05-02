9a833021f5f73157d95654de9f96b14dfbebd7a1
Leo-taro FUJIMOTO <lfujimot@e1r3p15.42.fr>
2018-05-02 18:55:10 +0200

    o

--- dirvm/ft_sub.c
+++ dirvm/ft_sub.c
@@ -6,7 +6,7 @@
 /*   By: emerabet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/13 13:28:37 by emerabet          #+#    #+#             */
-/*   Updated: 2018/05/01 19:48:41 by lfujimot         ###   ########.fr       */
+/*   Updated: 2018/05/02 18:54:19 by lfujimot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ int			ft_sub(t_process *proc, t_vm *vm)
 	a = ft_getparvalue(proc, vm, 0, MEM_SIZE);
 	b = ft_getparvalue(proc, vm, 1, MEM_SIZE);
 	c = a - b;
-	ft_printf("a%d b%d c%d\n", a,b,c);
 	ft_writeinreg(proc, proc->par[2], c);
 	if (c == 0)
 		proc->carry = 1;
